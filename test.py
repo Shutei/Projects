09cb72c6f55b0e0914fa6de87bd6c8a790dabab6
Shutei <youraznfriendis@gmail.com>
2015-08-13 15:20:25 -0700

    Can Retweet, cant follow, gets banned

--- test.py
+++ test.py
@@ -1,12 +1,33 @@
-import tweepy
+#initial setup
+import tweepy, time, sys, json
 
-key = 'YH0Yk3ZJUiyCg72aDFjqjd8OC'
-secret = '1f2T9Bqgu3fsQm9fIIwIy2KanznQtiFYEhQAWjWsbKXTKg4bwW'
-token = '3314467434-Jj7A6dBueSzGbQebgLJtleR2HUmnY0HBOT4yzZ0'
-token_secret = 'e0aqxPYMnmPjehImsaByApIv0e5QfjSkm10xuu1vObDxD'
-
+key = 'VzwVkotHIbjr2Szpvvm7J6f8X'
+secret = 'To6Ls1g4rDprQ3oocUhsPSsFYDXgcRsfM3FcXnPE9HOwApQL7B'
+token = '3314467434-Xf69VmUj07HAKx24tzjk5JwsWiIplB2SIIfM1fG'
+token_secret = 'K2EZVfpF0N2QSNoUnrf6jZh10x2wY0wqMlt0Y2jda6qmQ'
 auth = tweepy.OAuthHandler(key, secret)
 auth.set_access_token(token, token_secret)
-API = tweepy.API(auth)
-API.update_status(status = "Beep Boop")
+api = tweepy.API(auth)
+#initial setup
+
+#Actual Program:
+#API.update_status(status = "Beep Boop")
+x = 0
+while x == 1:
+	results = api.search(q="retweet to win")
+	for result in results:
+		try:
+			api.retweet(result.id)
+			print result._json['text']
+			print 'success! sleeping'
+			for x in range(0, 30):
+				time.sleep(1)
+				print 'x'
+			#user_id.follow()
+			#print result
+			data = api.get_status(result.id)._json
+			user_id = data['entities']['user_mentions'][(0)]['id']
+		except:
+			print "error"
+			time.sleep(.03)
 
