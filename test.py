9401358079dce2de5b4f510f5dab15054c13c1ec
Shutei <youraznfriendis@gmail.com>
2015-08-13 18:45:39 -0700

    added some random crawling code, fixed test

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 #initial setup
 import tweepy, time, sys, json
 
-key = 'VzwVkotHIbjr2Szpvvm7J6f8X'
-secret = 'To6Ls1g4rDprQ3oocUhsPSsFYDXgcRsfM3FcXnPE9HOwApQL7B'
-token = '3314467434-Xf69VmUj07HAKx24tzjk5JwsWiIplB2SIIfM1fG'
-token_secret = 'K2EZVfpF0N2QSNoUnrf6jZh10x2wY0wqMlt0Y2jda6qmQ'
+key = 'hk28iR2vdOmpLLdOKzKGR69g3'
+secret = 'NZTOXtZIzH5v2idzJyaE6bpptJtme30zf2ypBaCOldfC8Wluza'
+token = '3314638471-fTScfbeQiXWwwP5HEUip7fyN9d40pHvN5rjAzuG'
+token_secret = 'pfM0lWbvQGX5MUjruyUj3pUgb2TbmHfs8UIQZAhTJYoPe'
 auth = tweepy.OAuthHandler(key, secret)
 auth.set_access_token(token, token_secret)
 api = tweepy.API(auth)
@@ -12,22 +12,22 @@ api = tweepy.API(auth)
 
 #Actual Program:
 #API.update_status(status = "Beep Boop")
-x = 0
+x = 1
 while x == 1:
 	results = api.search(q="retweet to win")
 	for result in results:
+		time.sleep(2)
 		try:
-			api.retweet(result.id)
-			print result._json['text']
+			api.retweet(result._json['entities']['media'][(0)]['source_status_id'])
 			print 'success! sleeping'
 			for x in range(0, 30):
 				time.sleep(1)
-				print 'x'
-			#user_id.follow()
-			#print result
+				print x
+			user_id.follow()
+			print result
 			data = api.get_status(result.id)._json
 			user_id = data['entities']['user_mentions'][(0)]['id']
 		except:
 			print "error"
-			time.sleep(.03)
+
 
